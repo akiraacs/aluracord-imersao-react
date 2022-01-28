764ca0bec5dbb4b20fb19370989a5203c3917ea0
unknown <you@example.com>
2022-01-28 00:38:09 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from './config.json';
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 function Titulo(props) {
     console.log(props);
@@ -62,11 +35,13 @@ function Titulo(props) {
 //export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'akiraacs';
+    // const username = 'akiraacs';
+    const [username, setUsername] = React.useState('akiraacs');
+    const roteamento = useRouter();
+
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -93,6 +68,12 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento) {
+                            infosDoEvento.preventDefault();
+                            console.log('Alguém submeteu o form');
+                            roteamento.push('/chat');
+                            // window.location.href = '/chat';
+                          }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -103,11 +84,33 @@ export default function PaginaInicial() {
                             {appConfig.name}
                         </Text>
 
+                        {/* <input
+                                    type="text"
+                                    value={username}
+                                    onChange={function (event) {
+                                        console.log('usuario digitou', event.target.value);
+                                        // Onde ta o valor?
+                                        const valor = event.target.value;
+                                        // Trocar o valor da variavel
+                                        // através do React e avise quem precisa
+                                        setUsername(valor);
+                                    }}
+                                /> */}
+
                         <TextField
+                            value={username}
+                            onChange={function (event) {
+                            console.log('usuario digitou', event.target.value);
+                            // Onde ta o valor?
+                            const valor = event.target.value;
+                            // Trocar o valor da variavel
+                            // através do React e avise quem precisa
+                            setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
-                                    textColor: appConfig.theme.colors.neutrals[90],
+                                    textColor: appConfig.theme.colors.neutrals[200],
                                     mainColor: appConfig.theme.colors.neutrals[900],
                                     mainColorHighlight: appConfig.theme.colors.primary[600],
                                     backgroundColor: appConfig.theme.colors.neutrals[800],
