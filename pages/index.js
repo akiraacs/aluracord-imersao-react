348de356e76fffd8ca043dfcdd68ff7d1980df88
unknown <you@example.com>
2022-02-02 21:35:01 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import React from 'react';
 import { useRouter } from 'next/router';
-import appConfig from './config.json';
+import appConfig from '../config.json';
 
 function Titulo(props) {
     console.log(props);
@@ -21,31 +21,16 @@ function Titulo(props) {
 
 }
 
-//componente react
-//function HomePage() {
-//    // JSX
-//    return (
-//        <div>
-//            <GlobalStyle />
-//            <Titulo tag="h2">Boas vindas de volta!</Titulo>
-//            <h2>Discord - Alura Matrix</h2>
-//        </div>
-//    )
-//}
-//export default HomePage
-
 export default function PaginaInicial() {
-    // const username = 'akiraacs';
     const [username, setUsername] = React.useState('akiraacs');
     const roteamento = useRouter();
-
 
     return (
         <>
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
-                    backgroundColor: appConfig.theme.colors.primary[0],
+                    backgroundColor: appConfig.theme.colors.neutrals[200],
                     backgroundImage: 'url(https://a-static.besthdwallpaper.com/itachi-uchiha-papel-de-parede-2160x1440-2372_40.jpg)',
                     backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
                 }}
@@ -60,9 +45,10 @@ export default function PaginaInicial() {
                             sm: 'row',
                         },
                         width: '100%', maxWidth: '700px',
+                        border: `5px solid ${appConfig.theme.colors.primary['600']}`,
                         borderRadius: '5px', padding: '32px', margin: '16px',
                         boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                        backgroundColor: appConfig.theme.colors.primary[900],
+                        backgroundColor: appConfig.theme.colors.neutrals["opacity"],
                     }}
                 >
                     {/* Formulário */}
@@ -71,31 +57,17 @@ export default function PaginaInicial() {
                         onSubmit={function (infosDoEvento) {
                             infosDoEvento.preventDefault();
                             console.log('Alguém submeteu o form');
-                            roteamento.push('/chat');
-                            // window.location.href = '/chat';
+                            roteamento.push(`/chat?username=${username}`);
                           }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
                         }}
                     >
-                        <Titulo tag="h1">Que bom ver você por aqui!</Titulo>
+                        <Titulo tag="h1">Que bom ver você aqui!</Titulo>
                         <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[100] }}>
                             {appConfig.name}
                         </Text>
-
-                        {/* <input
-                                    type="text"
-                                    value={username}
-                                    onChange={function (event) {
-                                        console.log('usuario digitou', event.target.value);
-                                        // Onde ta o valor?
-                                        const valor = event.target.value;
-                                        // Trocar o valor da variavel
-                                        // através do React e avise quem precisa
-                                        setUsername(valor);
-                                    }}
-                                /> */}
 
                         <TextField
                             value={username}
